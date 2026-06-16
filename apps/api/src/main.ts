62ac4b8876def1a7239a3dd66be6bb88af9040b2
unknown <denniswang.pro@gmail.com>
2026-06-16 13:57:27 -0700

    update

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,9 +5,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
+
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ limit: '10mb', extended: true }));
 
   app.use(helmet());
   app.use(cookieParser());
